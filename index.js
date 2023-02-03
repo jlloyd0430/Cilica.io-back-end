d8a8a8e3b17489a12a60958a328c6c50e8a7ec52
jesse Lloyd <jesselloyd911@gmail.com>
2023-02-03 18:03:17 -0500

    refactor index

--- index.js
+++ index.js
@@ -14,7 +14,26 @@ dotenv.config();
 // the extended: true precises that the req.body object will contain values of any type instead of just strings.
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+const corsOptions = {
+  origin: "*",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 // localhost:3001/posts
 app.use("/posts", postRoutes);
